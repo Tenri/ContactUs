cd6d954af93658bb70e42b20109222e3d1640472
Hina Lee <hinawork@Hinas-MacBook-Pro.local>
2026-06-25 17:23:58 +1000

    home screen cleanup

--- contact-us-page/app/page.tsx
+++ contact-us-page/app/page.tsx
@@ -4,12 +4,16 @@ import Link from 'next/link';
 export default function Home() {
 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center ">
-        <h1>Welcome to OpenAgent!</h1>
-        <div className="flex flex-row items-center gap-6 text-center sm:items-start sm:text-left w-full h-full">
-            <Link href="/contact-us">Contact Us</Link>
-            <Link href="/contact-list">Contact List</Link>
+    <div className="min-h-screen bg-zinc-50 font-sans">
+      <main className="w-full max-w-6xl mx-auto px-16 py-8 ">
+        <h1 className="text-3xl font-bold py-15">Welcome to OpenAgent!</h1>
+        <div className="flex flex-row items-center gap-12 text-center sm:items-start sm:text-left w-full h-full">
+            <Link href="/contact-us" className="text-green-600 text-lg hover:underline">
+                Contact Us
+            </Link>
+            <Link href="/contact" className="text-green-600 text-lg hover:underline">
+                Contact List
+            </Link>
         </div>
       </main>
     </div>
